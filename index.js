0da9d300db2b358239dd4f5af432da0c2353df6d
Alexander Christie <alexanderchristie@outlook.com>
2014-09-25 04:08:58 +0100

    Export JSX Transpile

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ exports.browserify = require('./tasks/browserify')
 exports.devServer = require('./tasks/devServer')
 
 exports.sass = require('./tasks/sass')
+
+exports.jsxtranspile = require('./tasks/jsxTranspile')
